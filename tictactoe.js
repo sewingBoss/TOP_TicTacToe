c22b334166f25e8fce763b7d7f6acbd076b42686
Mana <nihs1b28@gmail.com>
2025-05-15 09:34:22 +0900

    convert from factory function to class

--- tictactoe.js
+++ tictactoe.js
@@ -1,52 +1,53 @@
 //gameboard stuff
-const gameboard = function(){
-    let arr = [
-        ['','',''], //row0
-        ['','',''], //row1
-        ['','','']  //row2
-    ];
-    let turn = 'O';
-    let winner;
-
-    function nxtTurn(){
-        if(turn == 'X'){
-            turn = 'O';
-        } else {
-            turn = 'X';
-        }
-        console.log(`next turn is ${turn}!`);
-    };
-
-    function setCell(row, col){
-        arr[row][col] = turn;
-        console.log(`row ${row} and col ${col} set to ${turn}`);
-    };
-
-    function getCell(row,col){
-        return arr[row][col];
-    };
-    function getTurn(){
-        return turn;
+class Gameboard{
+    constructor(){
+        this.arr = [
+            ['','',''], //row0
+            ['','',''], //row1
+            ['','','']  //row2
+        ];
+        this.turn = 'O';
+        this.winner = null;
     }
-    function someoneWon(){
-        if(winner){
-            console.log(`${winner} wins!`);
+
+    nxtTurn(){
+        this.turn = this.turn == 'X'? 'O': 'X';
+        console.log(`next turn is ${this.turn}!`);
+    }
+
+    setCell(row,col){
+        this.arr[row][col] = this.turn;
+        console.log(`row ${row} and col ${col} set to ${this.turn}`);
+    }
+
+    getCell(row, col){
+        return this.arr[row][col];
+    }
+
+    getTurn(){
+        return this.turn;
+    }
+
+    someoneWon(){
+        if(this.winner){
+            console.log(`${this.winner} wins!`);
             const winscreen = document.querySelector('.youwin');
             winscreen.classList.remove('invisible');
             winscreen.classList.add('playagain');
 
             const winpara = winscreen.querySelector('p');
-            winpara.textContent = `${winner} Wins !`
+            winpara.textContent = `${this.winner} Wins !`
             return;
         }
     }
-    function checkWin(){
+
+ checkWin(){
         //horizontal check
         out:for(let i = 0; i<3; i++){
             let prevVal;
             let bool = true;
             for(let j = 0; j<3; j++){
-                let currVal = arr[i][j];
+                let currVal = this.arr[i][j];
 
                 if(prevVal === "" || currVal === "") break;
                 if(j==0) {
@@ -59,19 +60,19 @@ const gameboard = function(){
                 if(!bool){
                     break;
                 } else if (j==2){
-                    winner = currVal;
+                    this.winner = currVal;
                     break out;
                 }
                 prevVal = currVal;
             }
         };
-someoneWon();
+this.someoneWon();
         //vertical check
         out:for(let i = 0; i<3; i++){
             let prevVal;
             let bool = true;
             for(let j = 0; j<3; j++){
-                let currVal = arr[j][i];
+                let currVal = this.arr[j][i];
 
                 if(prevVal === "" || currVal === "") break;
                 if(j==0) {
@@ -84,13 +85,13 @@ someoneWon();
                 if(!bool){
                     break;
                 } else if (j==2){
-                    winner = currVal;
+                    this.winner = currVal;
                     break out;
                 }
                 prevVal = currVal;
             }
         };
-someoneWon();
+this.someoneWon();
 
         //diagonal check left to right
         {
@@ -98,63 +99,63 @@ someoneWon();
             let bool = true;
 
             for(let i = 0;i<3;i++){
-                let currVal = arr[i][i];
+                let currVal = this.arr[i][i];
                 if(!(i==0)){bool = (currVal === prevVal)};
                 if(!bool){
                     break;
                 } else if(i == 2){
-                    winner = currVal;
+                    this.winner = currVal;
                 }
                 prevVal = currVal;
              }
         }
-someoneWon();
+this.someoneWon();
         //diagonal check right to left
         {
             let prevVal;
             let bool = true;
 
             for(let i = 2;i>-1;i--){
-                let currVal = arr[i][2-i];
+                let currVal = this.arr[i][2-i];
                 if(!(i==2)){bool = (currVal === prevVal)};
                 if(!bool){
                     break;
                 } else if(i == 0){
-                    winner = currVal;
+                    this.winner = currVal;
                 }
                 prevVal = currVal;
              }
         }
-someoneWon();
+this.someoneWon();
     };
-
-
-    return {
-        arr,
-        setCell,
-        getCell,
-        checkWin,
-        getTurn,
-        nxtTurn
-    };
-}();
-
+}
+const gameboard = new Gameboard();
 
 
 //player stuff
-function playerFactory(pname){
-    const name = pname;
-    let wins = 0;
-
-    function win(){
-        wins ++;
+class Player{
+    constructor(name){
+        this._name = name;
+        this._wins = 0;
     }
 
-    return {
-        name,
-        win
+    set wins(placeholder){
+        this._wins ++;
+    }
+
+    get wins(){
+        return this._wins;
+    }
+
+    get win(){
+        this.wins(0); //should work as wins setter. We need to use this
+    }
+    
+    get name(){
+        return this._name;
     }
 }
+
 
 //player form stuff
 const player1form = document.querySelector('#player1');
@@ -164,7 +165,7 @@ player1Button.addEventListener('click', (e)=>{
     e.preventDefault();
     const formdata = new FormData(player1form);
     let p1name = formdata.get('name');
-    if(player1obj===undefined){ player1obj = playerFactory(p1name);}
+    if(player1obj===undefined){ player1obj = new Player(p1name);}
     player1form.classList.add('invisible');
     console.log(player1form.classList.forEach((i)=>{console.log(i)}));
 });
@@ -178,7 +179,7 @@ player2Button.addEventListener('click', (e)=>{
     let p1name = formdata.get('name');
 
     let p2name = e.target.name;
-    if(player2obj===undefined){ player2obj = playerFactory(p2name);}
+    if(player2obj===undefined){ player2obj = new Player(p2name);}
     player2form.classList.add('invisible');
 });
 
